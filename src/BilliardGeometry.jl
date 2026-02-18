340934156e549dcd02fc30fe5ed9709815cd21c6
MathiasTensor <tensor1995@gmail.com>
2026-02-18 17:15:00 +0100

    Made neccesery changes into the dev branch

--- src/BilliardGeometry.jl
+++ src/BilliardGeometry.jl
@@ -33,6 +33,8 @@ export is_overlaping,connect_curves,is_connected,is_closed,angle,signed_angle,fi
 include("geometry/geometry.jl")
 export curvature,is_inside,domain_gradient_vector
 include("interpolations/cheb_interp.jl")
+include("geometry/boundarytypes.jl")
+export Transparent,PeriodicX,ReflectionSymmetry,QuantumSolverIgnore,get_boundary_curves,get_all_domains,get_domain,get_all_curves,get_curve,update_boundary_condition,SpecularReflection
 
 include("geometry/segments/linesegment.jl")
 export LineSegment,curve,domain_fun,arc_length,tangent,arc_length
@@ -48,11 +50,17 @@ include("geometry/domains/compositedomains.jl")
 export SimpleDomain,CompositeDomain,reset_ids!
 include("geometry/domains/polygons.jl")
 export Polygon
+include("geometry/billiards/limacon.jl")
+export LimaconSegment
+include("geometry/billiards/mushroom.jl")
+export MushroomBilliard
+include("geometry/billiards/stadium.jl")
+export StadiumBilliard
+include("geometry/billiards/triangle.jl")
+export TriangleBilliard
 
 include("geometry/arclength.jl")
 export _arc_length_integrand,construct_arc_length_interpolation
-include("geometry/boundarytypes.jl")
-export Transparent,PeriodicX,ReflectionSymmetry,QuantumSolverIgnore,get_boundary_curves,get_all_domains,get_domain,get_all_curves,get_curve,update_boundary_condition
 include("geometry/inversions.jl")
 export invert_curve,invert_curve_roots,invert_curve_optim,invert_curve_grid_search,invert_curve_nelder_mead
 include("geometry/poincarebirkhoff.jl")
