a569b6a73cc0ec59df68ce5c6a0e6806c0d3fdf8
MathiasTensor <tensor1995@gmail.com>
2026-09-16 15:54:06 +0200

    Update limacon.jl
    old stuff gone

--- src/geometry/billiards/limacon.jl
+++ src/geometry/billiards/limacon.jl
@@ -1,81 +1,21 @@
-
-
-function limacon_eq(a::T, R::T, arc_angle::T, shift_angle::T, center::SVector{2,T}, t) where T<:Real
-    #only shift angle = 0 is supported
-    s, c = sincos(arc_angle*t+shift_angle)
-    return SVector(R*(one(a)+a*c)*c - center[1], R*(one(a)+a*c)*s - center[2])
-end
-
-
-function limacon_arc_length(a, R, phi)
-    m = 4*a/(one(a)+a)^2;
-    res = Elliptic.E(phi, m)
-    return 2.0 *(1.0 + a) * res
-end
-
-struct LimaconSegment{T,BC} <: AbsPolarCurve{BC} where T<:Real
-    parameter::T
-    R::T
-    arc_angle::T
-    shift_angle::T
-    center::SVector{2,T}
-    orientation::Int64
-    length::T
-    cusp::SVector{2,T}
-    start::SVector{2,T}
-    bc::BC
-    domain_id::Int64
-    segment_id::Int64
-end
-
-function LimaconSegment(a; orientation=1, bc = SpecularReflection(), domain_id=1, segment_id=1 )
-    type = typeof(a)
-    R = one(type)
-    arc = type(pi)
-    shift = zero(type)
-    center = SVector(zero(type),zero(type))
-    L = 0.0
-    
-    limacon = LimaconSegment(a, R, arc, shift, center, orientation, L, center, center, bc, domain_id, segment_id)
-    r0, r1 = curve(limacon, [0.0,1.0])
-    center = (r0 .+ r1)/2
-    #recenter
-    limacon = LimaconSegment(a, R, arc, shift, -center, orientation, L, r1, r0, bc, domain_id, segment_id)
-    r0, r1 = curve(limacon, [0.0,1.0]) 
-    limacon = LimaconSegment(a, R, arc, shift, -center, orientation, L, r1, r0, bc, domain_id, segment_id)   
-    L = arc_length(limacon, 1.0)
-    return @set limacon.length = L
-end
-
-function polar_radius(limacon::L, phi::T) where {L<:LimaconSegment, T<:Real}
-   return limacon.R*(one(phi)+limacon.parameter *cos(phi))
-end
-
-# arc length
-#=
-function arc_length(limacon::L, pt::SVector{2,T}) where {L<:LimaconSegment, T<:Real}
-    let center = limacon.center, a=limacon.parameter, R=limacon.R
-        pt_polar = PolarFromCartesian()(Translation(-center)(pt))
-        return limacon_arc_length(a,R,pt_polar.θ)
-    end
-end
-=#
-
-####################################################################################
+# Limaçon billiard r(φ) = R(1 + a*cos(φ)); x-axis reflection symmetry.
 
 struct LimaconBilliard{T} <: AbsBilliard where T<:Real
-    fundamental_domain::PolarDomain{T}
+    fundamental_domain::SimpleDomain{T}
     symmetries::SymmetryRegistry
 end
 
+function LimaconBilliard(a::T; R::T = one(T)) where T<:Real
+    center = SVector{2,T}(zero(T), zero(T)); coef = SVector{2,T}(zero(T), a)
+    bc = SpecularReflection(); symmetries = register_symmetries(XAxisReflection())
 
-function LimaconBilliard(a)
-    limacon = LimaconSegment(a)
-    r0 = limacon.cusp
-    r1 = limacon.start
-    type = typeof(a)
-    symmetries = register_symmetries(XAxisReflection())
-    x_segment = LineSegment(r0, r1; bc=SymmetryWall(1,2),segment_id=2)
-    limacon_dom =  PolarDomain{type}([limacon,x_segment],[r1,r0],1)
-    return LimaconBilliard{type}(limacon_dom, symmetries)
+    arc = FourierCoeffPolarSegment(coef; R = R, arc_angle = T(pi), shift_angle = zero(T),
+        center = center, bc = bc, domain_id = 1, segment_id = 1)
+
+    p0 = curve(arc, zero(T)); p1 = curve(arc, one(T))
+    wall = LineSegment(p1, p0; bc = SymmetryWall(1, 2), domain_id = 1, segment_id = 2)
+
+    boundary = AbsCurve[arc, wall]; vertices = SVector{2,T}[p0, p1]
+    domain = SimpleDomain{T}(boundary, vertices, 1)
+    return LimaconBilliard{T}(domain, symmetries)
 end
