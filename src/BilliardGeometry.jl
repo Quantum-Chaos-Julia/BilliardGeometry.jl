9c39a23e9a94dafa4d4d84e9e0e686fb0cafec2b
MathiasTensor <tensor1995@gmail.com>
2026-09-21 10:22:59 +0200

    broken geom

--- src/BilliardGeometry.jl
+++ src/BilliardGeometry.jl
@@ -43,8 +43,6 @@ export kress_R!, kress_R_even!, kress_R_odd!, s_mid, kress_graded_nodes_data, mu
 include("geometry/billiards/sinai.jl")
 export SinaiBilliard
 
-include("geometry/billiards/rectangle_in_rectangle.jl")
-export RectangleWithinRectangleBilliard
 
 
 end
